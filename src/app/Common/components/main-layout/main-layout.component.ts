f026f82bdd83df8be723c2049ab13c5e2bf40427
Bruno Rocha <bruno.rocha.dev@outlook.com>
2024-04-20 16:52:15 -0300

    Adding the FontAwesome package to the project and passing the icon by parameter to the heade

--- src/app/Common/components/main-layout/main-layout.component.ts
+++ src/app/Common/components/main-layout/main-layout.component.ts
@@ -1,19 +1,22 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Data, Router, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Data, RouterOutlet } from '@angular/router';
 import { ProfileLoggedComponent } from '../profile-logged/profile-logged.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AuthService } from '../../../modules/auth/services/auth.service';
 import { Subscription } from 'rxjs';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet, ProfileLoggedComponent],
+  imports: [RouterOutlet, ProfileLoggedComponent, FontAwesomeModule],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
 export class MainLayoutComponent {
   private subscription = new Subscription();
   pageTitle: string | undefined;
+  currentIcon!: IconProp;
   userName: string | undefined;
   avatarUrl: string | undefined;
   role: string | undefined;
@@ -34,6 +37,7 @@ export class MainLayoutComponent {
 
     this.route.firstChild?.data.subscribe((data: Data) => {
       this.pageTitle = data['pageTitle'];
+      this.currentIcon = data['icon'];
     });
   }
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './guards/auth.guard';
 import { MainLayoutComponent } from './Common/components/main-layout/main-layout.component';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 
 export const routes: Routes = [
   {
@@ -17,7 +18,7 @@ export const routes: Routes = [
         loadChildren: () =>
           import('./modules/home/home.module').then((m) => m.HomeModule),
         canActivate: [authGuard],
-        data: { pageTitle: 'Dashboard', icon: 'home-icon' },
+        data: { pageTitle: 'Home', icon: faHome },
       },
     ],
   },
