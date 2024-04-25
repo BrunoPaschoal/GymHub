a1ea7ff564a5221e2bfdbcb1d24c8c615ee4686f
Bruno Rocha <bruno.rocha.dev@outlook.com>
2024-04-24 21:19:58 -0300

    Fix root path redirect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,11 @@ import { faAddressCard, faHome } from '@fortawesome/free-solid-svg-icons';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
     path: 'auth',
     loadChildren: () =>
       import('./modules/auth/auth.module').then((m) => m.AuthModule),
